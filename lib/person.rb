9e5942f030ee09338a11d0903ee4ab22dba8e52f
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-01-05 23:35:36 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -7,7 +7,9 @@ end
 
 
 class Person
-  attr_accessor :name, :age
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, 
+:weight, :handed, :complexion, :t_shirt_size, 
+:wrist_size, :glove_size, :pant_length, :pant_width
  
   def initialize(name:, age:)
     @name = name
